84626e449cb6114fca13858ca5e1140694cca2fa
daiane.bellon <daianebellon@hotmail.com>
2022-04-26 13:51:06 -0300

    #4 - Corrigindo exception

--- src/main/java/br/com/daianebellon/userservice/pessoa/validacoes/IdValidation.java
+++ src/main/java/br/com/daianebellon/userservice/pessoa/validacoes/IdValidation.java
@@ -1,10 +1,12 @@
 package br.com.daianebellon.userservice.pessoa.validacoes;
 
+import br.com.daianebellon.userservice.pessoa.exceptions.CampoInvalidoException;
+
 public class IdValidation {
 
     public static void validar(Long id) {
         if (id == null) {
-            throw new NullPointerException("Id inválido"); //criar exception
+            throw new CampoInvalidoException("Id inválido");
         }
     }
 }
